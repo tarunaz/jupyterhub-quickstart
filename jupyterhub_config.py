f95b36fffc714e42de95b3436cf8fcac742d9053
tarunaz <tmehra@redhat.com>
2018-03-19 12:14:15 -0700

    Update jupyterhub_config.py

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -74,6 +74,8 @@ class DemoFormSpawner(LocalProcessSpawner):
     def _options_form_default(self):
         default_env = "YOURNAME=%s\n" % self.user.name
         return """
+        <label for="args">Extra notebook CLI arguments</label>
+        <input name="args" placeholder="e.g. --debug"></input>
         <label for="env">Environment variables (one per line)</label>
         <textarea name="env">{env}</textarea>
         """.format(env=default_env)
@@ -88,14 +90,23 @@ class DemoFormSpawner(LocalProcessSpawner):
                 key, value = line.split('=', 1)
                 env[key.strip()] = value.strip()
         
+        arg_s = formdata.get('args', [''])[0].strip()
+        if arg_s:
+            options['argv'] = shlex.split(arg_s)
         return options
-      
+    
+    def get_args(self):
+        """Return arguments to pass to the notebook server"""
+        argv = super().get_args()
+        if self.user_options.get('argv'):
+            argv.extend(self.user_options['argv'])
+        return argv
+    
     def get_env(self):
         env = super().get_env()
         if self.user_options.get('env'):
             env.update(self.user_options['env'])
         return env
-    
 
 c.JupyterHub.spawner_class = DemoFormSpawner
 
